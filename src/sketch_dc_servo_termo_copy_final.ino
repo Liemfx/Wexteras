44985161f725094d3bbc0d4728b134613ef30cb0
benib0 <benjung1067@gmail.com>
2023-10-23 09:56:08 +0200

    Update sketch_dc_servo_termo_copy_final.ino
    gjorde om den med den nya motorn

--- src/sketch_dc_servo_termo_copy_final.ino
+++ src/sketch_dc_servo_termo_copy_final.ino
@@ -3,58 +3,51 @@
 #include <Adafruit_Sensor.h>
 #include <ESP8266WiFi.h>
 #include <DHT.h>
-//Provide the token generation process info.
 #include "addons/TokenHelper.h"
-//Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
- 
+#include <TimeAlarms.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 #define WIFI_SSID "ABB_Gym_IOT"
 #define WIFI_PASSWORD "Welcome2abb"
-
 #define API_KEY "AIzaSyCJO5qCb3U2pxThR1h0ZcNhw25za4rGNfY"
-
-#define DATABASE_URL "https://wexteras-blv-default-rtdb.europe-west1.firebasedatabase.app/" 
-
+#define DATABASE_URL "https://wexteras-blv-default-rtdb.europe-west1.firebasedatabase.app/"
 bool signupOK = false;
-//Define Firebase Data object
+
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
 Servo servo;
 
- 
+#define motorPinRightDirA D0 // D2 for Motor A
+#define motorPinRightSpeedA D1 // D1 for Motor A
 
-#define motorPinRightDir  D2 // D2
+#define motorPinRightDirB D2 // D3 for Motor B
+#define motorPinRightSpeedB D3 // D4 for Motor B
 
-#define motorPinRightSpeed D1 // D1 
-
-#define DHTPIN D5 // Pin where your DHT11 sensor is connected (D5 on ESP8266)
-
-#define DHTTYPE DHT11 // Specify the DHT sensor type
-
- 
+#define DHTPIN D5
+#define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
-
- 
-
-int servoOpenAngle = 180;
-
+WiFiUDP udp;
+int servoOpenAngle = 190;
 int servoClosedAngle = 0;
-
 bool luckaOpen = false;
-
 bool flaktPa = false;
- 
+const int startHour = 12; // 12:00 PM
+const int startMinute = 0;
+const long utcOffsetInSeconds = 7200;
+const char* ntpServerName = "pool.ntp.org";
+const int ntpServerPort = 123;
+NTPClient timeClient(udp, ntpServerName, utcOffsetInSeconds, ntpServerPort);
+bool motorStarted = false;
 
 void setup() {
-
-
   Serial.begin(115200);
-   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(300);
   }
@@ -63,99 +56,114 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.println();
 
-  servo.attach(2); // Attach servo to pin 2 (D4)
+  servo.attach(D6); // Attach servo to pin D6
 
-  pinMode(motorPinRightDir, OUTPUT);
+  pinMode(motorPinRightDirA, OUTPUT);
+  pinMode(motorPinRightSpeedA, OUTPUT);
 
-  pinMode(motorPinRightSpeed, OUTPUT);
- 
+  pinMode(motorPinRightDirB, OUTPUT);
+  pinMode(motorPinRightSpeedB, OUTPUT);
+
   config.api_key = API_KEY;
-  
   config.database_url = DATABASE_URL;
 
-if (Firebase.signUp(&config, &auth, "", "")){
-    Serial.println("ok");
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("Firebase Sign Up successful.");
     signupOK = true;
-  }
-  else{
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  } else {
+    Serial.printf("Firebase Sign Up Error: %s\n", config.signer.signupError.message.c_str());
   }
 
   dht.begin();
 
-
-
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 
+  timeClient.begin();
+  timeClient.setTimeOffset(7200);
+  Serial.begin(115200);
 }
 
- 
-
 void loop() {
+timeClient.update();
 
-
-
-
+  // Get the current time
+  int currentHour = timeClient.getHours();
+  int currentMinute = timeClient.getMinutes();
   int temperature = dht.readTemperature();
   int humidity = dht.readHumidity();
-  
+
+  Serial.print("Temperature: ");
   Serial.print(temperature);
-
   Serial.print("°C, Humidity: ");
-
   Serial.print(humidity);
-
   Serial.println("%");
 
- Firebase.RTDB.setInt(&fbdo, "dht/temperature", temperature); 
- Firebase.RTDB.setInt(&fbdo, "dht/humidity", humidity);   
- Firebase.RTDB.setBool(&fbdo, "dht/luckaopen", luckaOpen);   
- Firebase.RTDB.setBool(&fbdo, "dht/flaktpa", flaktPa); 
-  Serial.print("Temperature: ");
-
-  
- 
+  Firebase.RTDB.setInt(&fbdo, "dht/temperature", temperature);
+  Firebase.RTDB.setInt(&fbdo, "dht/humidity", humidity);
+  Firebase.RTDB.setBool(&fbdo, "dht/luckaopen", luckaOpen);
+  Firebase.RTDB.setBool(&fbdo, "dht/flaktpa", flaktPa);
 
   if (isnan(temperature) || isnan(humidity)) {
-
     Serial.println("Failed to read from DHT sensor!");
+  }
+   
+// vattna 1 gång om dagen
+  if (!motorStarted && currentHour == startHour && currentMinute == startMinute) {
+  digitalWrite(motorPinRightDirA, 1); // Set motor A direction forward
+   analogWrite(motorPinRightSpeedA, 100); // Set motor A speed
+   delay(1500);
+   digitalWrite(motorPinRightDirA, 0); // Set motor A direction forward
+   analogWrite(motorPinRightSpeedA, 0);
+   delay(1500);
+   digitalWrite(motorPinRightDirA, 1); // Set motor A direction forward
+   analogWrite(motorPinRightSpeedA, 100); // Set motor A speed
+   delay(1500);
 
+   digitalWrite(motorPinRightDirA, 0); // Set motor A direction forward
+   analogWrite(motorPinRightSpeedA, 0);
+   delay(300);
   }
 
- 
 
-  // Motor and servo control based on temperature and humidity
 
-  if (temperature >= 26 && humidity > 60) {
-
-    digitalWrite(motorPinRightDir, 1); // Set motor direction
-
-    analogWrite(motorPinRightSpeed, 512); // Set motor 
-    
+  // Motor B control based on temperature and humidity
+  if (temperature >= 26 && humidity > 80) {
+    digitalWrite(motorPinRightDirB, 1); // Set motor B direction forward
+    analogWrite(motorPinRightSpeedB, 1000); // Set motor B speed
     flaktPa = true;
-  
-  } else {
-
-    digitalWrite(motorPinRightDir, 0);
-
-    analogWrite(motorPinRightSpeed, 0);
-
+  }
+ else {
+    digitalWrite(motorPinRightDirB, 0);
+    analogWrite(motorPinRightSpeedB, 0);
     flaktPa = false;
   }
 
- 
 
-  if (humidity > 60) {
+  //vattna om det blir för torrt inne
+   if (humidity < 25) {
+   digitalWrite(motorPinRightDirA, 1); // Set motor A direction forward
+   analogWrite(motorPinRightSpeedA, 100); // Set motor A speed
+   delay(1500);
+   digitalWrite(motorPinRightDirA, 0); // Set motor A direction forward
+   analogWrite(motorPinRightSpeedA, 0);
+   delay(1500);
+   digitalWrite(motorPinRightDirA, 1); // Set motor A direction forward
+   analogWrite(motorPinRightSpeedA, 100); // Set motor A speed
+   delay(1500);
 
+   digitalWrite(motorPinRightDirA, 0); // Set motor A direction forward
+   analogWrite(motorPinRightSpeedA, 0);
+   delay(300);
+  }   
+  if (humidity > 80) {
     servo.write(servoOpenAngle);
     luckaOpen = true;
   } 
-  else {
-
+  else{
     servo.write(servoClosedAngle);
     luckaOpen = false;
   }
 
-
+  
 }
